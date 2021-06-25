c61d70d137e6b52fabdbbcae3a5f9f669b04b4f9
Jason Willmore <jason_m_willmore@homedepot.com>
2021-06-25 12:02:41 -0700

    add a bunch of services

--- src/services/index.js
+++ src/services/index.js
@@ -1,10 +1,19 @@
+import CallForPrice from './callForPrice.json';
 import CatalogGateway from './catalogGateway.json';
 import DiscountingAggregator from './discountingAggregator.json';
-
+import DiscountingICP from './discountingICP.json';
+import ExternalPricing from './externalPricing.json';
+import ProductRecommendationsACL from './productRecommendationsACL.json';
+import ProductSearch from './productSearch.json';
 
 export const services = [
+    CallForPrice,
     CatalogGateway,
     DiscountingAggregator,
+    DiscountingICP,
+    ExternalPricing,
+    ProductRecommendationsACL,
+    ProductSearch,
 ];
 
 export default services;
